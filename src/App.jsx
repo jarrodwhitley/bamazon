1d0bbdd2157800584e7067de748b0b49659aca57
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-11-23 15:50:17 -0600

    Fixed provider bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavigationBar from './components/NavigationBar.jsx';
 import Content from './components/Content.jsx';
 import Footer from './components/Footer.jsx';
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
-import {Context, useSelectedCategory, useIsFiltering, useSetIsFiltering, useSearchString} from "./components/ContextProvider.jsx";
+import {Context, useSelectedCategory, useIsFiltering, useSetIsFiltering } from "./components/ContextProvider.jsx";
 import productsData from './assets/data/products.json';
 
 export default function App() {
@@ -12,9 +12,7 @@ export default function App() {
     const [rawProducts, setRawProducts] = useState([]);
     const [selectedProduct, setSelectedProduct] = useState(null);
     const [filteredProducts, setFilteredProducts] = useState([]);
-    const setIsFiltering = useSetIsFiltering(false);
-    const searchString = useSearchString('');
-    const selectedCategory = useSelectedCategory({})
+    const selectedCategory = useSelectedCategory()
     useEffect(() => {
         const handleLoad = () => {
             setIsLoading(false);
@@ -34,28 +32,6 @@ export default function App() {
         return newObj;
     });
     
-    const filterProducts = (string) => {
-        console.log('Filtering products...', string);
-        if (!string) return;
-        if (string.length > 2) {
-            let filtered = products.filter(product => {
-                return product.title.toLowerCase().includes(string.toLowerCase()) ||
-                    product.tags.join(' ').toLowerCase().includes(string.toLowerCase()) ||
-                    (product.brand && product.brand.toLowerCase().includes(string.toLowerCase())) ||
-                    product.description.toLowerCase().includes(string.toLowerCase());
-            });
-            setFilteredProducts(filtered);
-            setIsFiltering(true);
-        } else {
-            setFilteredProducts(products);
-            setIsFiltering(false);
-        }
-    }
-    
-    useEffect(() => {
-        console.log('APP.jsx => Search string:', searchString);
-    }, [searchString]);
-    
     function enterPress() {
         console.log('Enter pressed');
     }
@@ -63,8 +39,8 @@ export default function App() {
     return (
         <ErrorBoundary>
             <Context products={products} filteredProducts={filteredProducts} selectedProduct={selectedProduct} selectedCategory={selectedCategory}>
-                <NavigationBar onEnterPress={enterPress} className="z-10"/>
-                <Content onFilterProducts={filterProducts} isLoading={isLoading} className="z-0"/>
+                <NavigationBar onEnterPress={enterPress} className={'z-10'}/>
+                <Content isLoading={isLoading} className="z-0"/>
                 <Footer/>
             </Context>
         </ErrorBoundary>
--- src/components/ContextProvider.jsx
+++ src/components/ContextProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const ProductsContext = createContext(null);
@@ -7,6 +7,7 @@ const SetSelectedProductContext = createContext(null);
 const SelectedCategoryContext = createContext(null);
 const SetSelectedCategoryContext = createContext(null);
 const FilteredProductsContext = createContext(null);
+const SetFilteredProductsContext = createContext(null);
 const FilteringContext = createContext(false);
 const SetFilteringContext = createContext(null);
 const SearchStringContext = createContext(null)
@@ -14,35 +15,34 @@ const SetSearchStringContext = createContext(null);
 
 export const Context = ({ products, filteredProducts, selectedProduct, selectedCategory, filtering, searchString, children }) => {
     const [selectedProductState, setSelectedProductState] = useState(selectedProduct);
+    const [filteredProductsState, setFilteredProductsState] = useState(filteredProducts);
     const [selectedCategoryState, setSelectedCategoryState] = useState(selectedCategory);
     const [filteringState, setFilteringState] = useState(filtering);
     const [searchStringState, setSearchStringState] = useState(searchString);
-    
-    // console.log("Selected Product State:", selectedProductState);
-    // console.log("Selected Category State:", selectedCategoryState);
-    // console.log("Filtering State:", filteringState);
-    console.log("ContextProvider.jsx => Search String State:", searchStringState);
+    const [isFilteringState, setIsFilteringState] = useState(false);
     
     return (
         <ProductsContext.Provider value={products}>
-            <FilteredProductsContext.Provider value={filteredProducts}>
-                <SelectedProductContext.Provider value={selectedProductState}>
-                    <SetSelectedProductContext.Provider value={setSelectedProductState}>
-                        <SelectedCategoryContext.Provider value={selectedCategoryState}>
-                            <SetSelectedCategoryContext.Provider value={setSelectedCategoryState}>
-                                <FilteringContext.Provider value={filteringState}>
-                                    <SetFilteringContext.Provider value={setFilteringState}>
-                                        <SearchStringContext.Provider value={searchStringState}>
-                                            <SetSearchStringContext.Provider value={setSearchStringState}>
-                                                {children}
-                                            </SetSearchStringContext.Provider>
-                                        </SearchStringContext.Provider>
-                                    </SetFilteringContext.Provider>
-                                </FilteringContext.Provider>
-                            </SetSelectedCategoryContext.Provider>
-                        </SelectedCategoryContext.Provider>
-                    </SetSelectedProductContext.Provider>
-                </SelectedProductContext.Provider>
+            <FilteredProductsContext.Provider value={filteredProductsState}>
+                <SetFilteredProductsContext.Provider value={setFilteredProductsState}>
+                    <SelectedProductContext.Provider value={selectedProductState}>
+                        <SetSelectedProductContext.Provider value={setSelectedProductState}>
+                            <SelectedCategoryContext.Provider value={selectedCategoryState}>
+                                <SetSelectedCategoryContext.Provider value={setSelectedCategoryState}>
+                                    <FilteringContext.Provider value={isFilteringState}>
+                                        <SetFilteringContext.Provider value={setIsFilteringState}>
+                                            <SearchStringContext.Provider value={searchStringState}>
+                                                <SetSearchStringContext.Provider value={setSearchStringState}>
+                                                    {children}
+                                                </SetSearchStringContext.Provider>
+                                            </SearchStringContext.Provider>
+                                        </SetFilteringContext.Provider>
+                                    </FilteringContext.Provider>
+                                </SetSelectedCategoryContext.Provider>
+                            </SelectedCategoryContext.Provider>
+                        </SetSelectedProductContext.Provider>
+                    </SelectedProductContext.Provider>
+                </SetFilteredProductsContext.Provider>
             </FilteredProductsContext.Provider>
         </ProductsContext.Provider>
     );
@@ -50,6 +50,7 @@ export const Context = ({ products, filteredProducts, selectedProduct, selectedC
 
 export const useProducts = () => useContext(ProductsContext);
 export const useFilteredProducts = () => useContext(FilteredProductsContext);
+export const useSetFilteredProducts = () => useContext(SetFilteredProductsContext);
 export const useSelectedProduct = () => useContext(SelectedProductContext);
 export const useSetSelectedProduct = () => useContext(SetSelectedProductContext);
 export const useSelectedCategory = () => useContext(SelectedCategoryContext);
