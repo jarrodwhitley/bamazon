07b2ec0fb975803110a8f79d1fa6bb309727f542
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-12-11 12:00:29 -0600

    redux updates

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,15 @@
-import { useSelector, useDispatch } from 'react-redux';
-import {setCart, updateItem, removeItem, clearCart} from '../store/cartSlice';
-import { useMemo, useState } from 'react';
-// import { useCart, useSetCart } from './ContextProvider.jsx';
-import Boombam from "../assets/images/bamazon_logo_boombam.png";
+import { useSelector, useDispatch } from 'react-redux'
+import { setCart, updateItem, removeItem, clearCart } from '../store/cartSlice'
+import { useMemo, useState } from 'react'
+import Boombam from '../assets/images/bamazon_logo_boombam.png'
 
 export default function Cart() {
-    const cart = useSelector(state => state.cart);
-    const dispatch = useDispatch();
-    const [showBam, setShowBam] = useState(false);
+    const cart = useSelector((state) => state.cart)
+    const dispatch = useDispatch()
+    const [showBam, setShowBam] = useState(false)
     const handleRemoveFromCart = (e, id) => {
-        dispatch(removeItem(id));
-    };
+        dispatch(removeItem(id))
+    }
     const handleQuantityChange = (e, id) => {
         // let newCart = [...cart.items];
         // let productIndex = newCart.findIndex(product => product.id === id);
@@ -19,80 +18,181 @@ export default function Cart() {
         //     showCart: cart.showCart,
         //     items: newCart
         // }));
-        dispatch(updateItem({id, quantity: parseInt(e.target.value)}));
+        dispatch(updateItem({ id, quantity: parseInt(e.target.value) }))
     }
     const handleCheckout = () => {
-        setShowBam(true);
+        setShowBam(true)
         setTimeout(() => {
-            dispatch(clearCart());
-            setShowBam(false);
-        },1000);
+            dispatch(clearCart())
+            setShowBam(false)
+        }, 1000)
     }
-    // calculate the total savings using each item's price and discount percentage
-    // Then add them all together
-    // use memo to prevent recalculation on every render
+
     const totalSavings = useMemo(() => {
         return cart.items.reduce((acc, product) => {
-            return acc + (product.price * product.discountPercentage / 100);
-        }, 0);
-    }, [cart.items]);
-    
+            return acc + (product.price * product.discountPercentage) / 100
+        }, 0)
+    }, [cart.items])
+
     return (
-        <div className={
-            'cart__container fixed w-full lg:w-[400px] h-full overflow-hidden pb-32 top-[100px] lg:top-[109px] right-0 bg-white shadow-lg animate__animated animate__faster z-10 px-4 ' +
-            (cart.showCart ? 'animate__slideInRight ' : 'animate__slideOutRight ')}>
-            { cart.items.length > 0 && (
-                <div className={'text-base lg:text-2xl font-semibold pl-6 py-2 lg:py-4 border-b'}>Your cart</div>
+        <div
+            className={
+                'cart__container fixed w-full lg:w-[400px] h-full overflow-hidden pb-32 top-[100px] lg:top-[109px] right-0 bg-white shadow-lg animate__animated animate__faster z-10 px-4 ' +
+                (cart.showCart
+                    ? 'animate__slideInRight '
+                    : 'animate__slideOutRight ')
+            }
+        >
+            {cart.items.length > 0 && (
+                <div
+                    className={
+                        'text-base lg:text-2xl font-semibold pl-6 py-2 lg:py-4 border-b'
+                    }
+                >
+                    Your cart
+                </div>
             )}
-            
+
             {/* Cart Items */}
-            <div className={'cart__items h-[calc(100%-64px)] pb-[100px] overflow-auto'}>
+            <div
+                className={
+                    'cart__items h-[calc(100%-64px)] pb-[100px] overflow-auto'
+                }
+            >
                 {cart.items.map((product, index) => (
-                    <div key={index} className={'cart__product grid grid-cols-[20%_1fr_auto_auto] items-center gap-4 p-4 border-b border-gray-200'}>
-                        <img src={product.images[0]} alt={product.title} className={'w-16 h-16 object-cover row-start-1'}/>
-                        <div className={'details flex flex-col row-start-1 self-start gap-2 leading-[1]'}>
-                            <span className={'text-sm whitespace-nowrap max-w-[150px] text-ellipsis overflow-hidden'}>{product.title}</span>
-                            <span className={'font-semibold'}>${product.price.toFixed(2)}</span>
+                    <div
+                        key={index}
+                        className={
+                            'cart__product grid grid-cols-[20%_1fr_auto_auto] items-center gap-4 p-4 border-b border-gray-200'
+                        }
+                    >
+                        <img
+                            src={product.images[0]}
+                            alt={product.title}
+                            className={'w-16 h-16 object-cover row-start-1'}
+                        />
+                        <div
+                            className={
+                                'details flex flex-col row-start-1 self-start gap-2 leading-[1]'
+                            }
+                        >
+                            <span
+                                className={
+                                    'text-sm whitespace-nowrap max-w-[150px] text-ellipsis overflow-hidden'
+                                }
+                            >
+                                {product.title}
+                            </span>
+                            <span className={'font-semibold'}>
+                                ${product.price.toFixed(2)}
+                            </span>
                         </div>
-                        <div className={'quantity flex items-center row-start-1 gap-2'}>
-                            <select className={'font-base bg-gray-100 rounded'} value={product.quantity} onChange={(e) => handleQuantityChange(e, product.id)}>
-                                {[...Array(10).keys()].map(i => (
-                                    <option key={i + 1} value={i + 1}>{i + 1}</option>
+                        <div
+                            className={
+                                'quantity flex items-center row-start-1 gap-2'
+                            }
+                        >
+                            <select
+                                className={'font-base bg-gray-100 rounded'}
+                                value={product.quantity || ''}
+                                onChange={(e) =>
+                                    handleQuantityChange(e, product.id)
+                                }
+                            >
+                                {[...Array(10).keys()].map((i) => (
+                                    <option key={i + 1} value={i + 1}>
+                                        {i + 1}
+                                    </option>
                                 ))}
                             </select>
                         </div>
-                        <i className={'fa-solid fa-trash-alt text-gray-300 cursor-pointer row-start-1'} onClick={(e) => handleRemoveFromCart(e, product.id)}></i>
+                        <i
+                            className={
+                                'fa-solid fa-trash-alt text-gray-300 cursor-pointer row-start-1'
+                            }
+                            onClick={(e) => handleRemoveFromCart(e, product.id)}
+                        ></i>
                     </div>
                 ))}
             </div>
-            
+
             {/* Subtotal and Checkout Button */}
             {cart.items.length > 0 && (
-                <div className={'flex flex-col justify-evenly items-center h-fit w-full bg-white p-4 lg:pb-10 shadow-2xl absolute top-auto left-0 right-0 bottom-[100px] overflow-auto shadow-[0_0_8px_rgba(0,0,0,0.2)]'}>
-                    <div className={'cart__savings w-full flex items-center justify-between pt-2'}>
-                        <span className={'text-sm font-semibold'}>Total Savings:</span>
-                        <span className={'font-semibold'}>-${totalSavings.toFixed(2)}</span>
+                <div
+                    className={
+                        'flex flex-col justify-evenly items-center h-fit w-full bg-white p-4 pr-6 lg:pb-10 shadow-2xl absolute top-auto left-0 right-0 bottom-[100px] overflow-auto shadow-[0_0_8px_rgba(0,0,0,0.2)]'
+                    }
+                >
+                    <div
+                        className={
+                            'cart__savings w-full flex items-center justify-between pt-2'
+                        }
+                    >
+                        <span className={'text-sm font-semibold'}>
+                            Total Savings:
+                        </span>
+                        <span className={'font-semibold'}>
+                            -${totalSavings.toFixed(2)}
+                        </span>
                     </div>
-                    <div className={'cart__subtotal w-full flex items-center justify-between pt-2'}>
-                        <span className={'text-sm font-semibold'}>Subtotal:</span>
-                        <span className={'font-semibold'}>${cart.items.reduce((acc, product) => acc + (product.price * product.quantity), 0).toFixed(2)}</span>
+                    <div
+                        className={
+                            'cart__subtotal w-full flex items-center justify-between pt-2'
+                        }
+                    >
+                        <span className={'text-sm font-semibold'}>
+                            Subtotal:
+                        </span>
+                        <span className={'font-semibold'}>
+                            $
+                            {cart.items
+                                .reduce(
+                                    (acc, product) =>
+                                        acc + product.price * product.quantity,
+                                    0
+                                )
+                                .toFixed(2)}
+                        </span>
                     </div>
-                    <div className={'cart__checkout w-fit h-fit mt-4 py-2 px-8 cursor-pointer text-white bg-orange-400 font-semibold rounded'} onClick={handleCheckout}>Checkout</div>
+                    <div
+                        className={
+                            'cart__checkout w-fit h-fit mt-4 py-2 px-8 cursor-pointer text-white bg-orange-400 font-semibold rounded'
+                        }
+                        onClick={handleCheckout}
+                    >
+                        Checkout
+                    </div>
                 </div>
             )}
-            
+
             {/* Cart empty */}
             {cart.items.length === 0 && (
-                <div className={'cart__empty flex flex-col items-center justify-center h-full select-none'}>
-                    <span className={'text-2xl font-semibold text-center'}>Your cart is empty 😅</span>
-                    <span className={'text-xl font-base text-center mt-4 mx-6 leading-[1]'}>Come back after you add a few things!</span>
+                <div
+                    className={
+                        'cart__empty flex flex-col items-center justify-center h-full select-none'
+                    }
+                >
+                    <span className={'text-2xl font-semibold text-center'}>
+                        Your cart is empty 😅
+                    </span>
+                    <span
+                        className={
+                            'text-xl font-base text-center mt-4 mx-6 leading-[1]'
+                        }
+                    >
+                        Come back after you add a few things!
+                    </span>
                 </div>
             )}
-            
+
             {/*  BAM  */}
             {showBam && (
-                <img src={Boombam} alt="Bamazon logo" className="animate__animated animate__bounceIn animate__faster single-product-view__bam fixed top-0 left-0 w-full drop-shadow-2xl z-10"/>
+                <img
+                    src={Boombam}
+                    alt="Bamazon logo"
+                    className="animate__animated animate__bounceIn animate__faster single-product-view__bam fixed top-0 left-0 w-full drop-shadow-2xl z-10"
+                />
             )}
         </div>
-    );
+    )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,113 +1,165 @@
-import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { setFilters, updateFilters, clearFilters, filtersActive } from "../store/filtersSlice";
-import { capitalizeFirstLetter } from "../utils/functions";
-import { filteredProducts } from "../store/productsSlice.js";
+import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+    setFilters,
+    updateFilters,
+    clearFilters,
+    filtersActive,
+} from '../store/filtersSlice'
+import { capitalizeFirstLetter } from '../utils/functions'
+import { filteredProducts } from '../store/productsSlice.js'
 
-export default function Sidebar({ isMobile }) {
-    const dispatch = useDispatch();
-    const filteredProductsState = useSelector(filteredProducts);
-    const selectedFilters = useSelector(state => state.filters);
-    const filtersActiveState = useSelector(filtersActive);
-    const [categories, setCategories] = useState([]);
-    const [brands, setBrands] = useState([]);
+export default function Sidebar() {
+    const dispatch = useDispatch()
+    const isMobile = useSelector((state) => state.ui.isMobile)
+    const filteredProductsState = useSelector(filteredProducts)
+    const selectedFilters = useSelector((state) => state.filters)
+    const filtersActiveState = useSelector(filtersActive)
+    const [categories, setCategories] = useState([])
+    const [brands, setBrands] = useState([])
     const prices = [
         { min: 0, max: 50 },
         { min: 51, max: 100 },
-        { min: 101, max: 1000 }
-    ];
-    const [showFilters, setShowFilters] = useState(false);
-    
+        { min: 101, max: 1000 },
+    ]
+    const [showFilters, setShowFilters] = useState(false)
+
     useEffect(() => {
         if (Array.isArray(filteredProductsState)) {
             if (categories.length < 1) {
-                const newCategories = filteredProductsState.reduce((acc, product) => {
-                    if (!acc.includes(product.category) && product.category !== undefined) {
-                        acc.push(product.category);
-                    }
-                    return acc;
-                }, []);
-                setCategories(newCategories);
+                const newCategories = filteredProductsState.reduce(
+                    (acc, product) => {
+                        if (
+                            !acc.includes(product.category) &&
+                            product.category !== undefined
+                        ) {
+                            acc.push(product.category)
+                        }
+                        return acc
+                    },
+                    []
+                )
+                setCategories(newCategories)
             }
-            
+
             if (brands.length < 1) {
-                const newBrands = filteredProductsState.reduce((acc, product) => {
-                    if (!acc.includes(product.brand) && product.brand !== undefined) {
-                        acc.push(product.brand);
-                    }
-                    return acc;
-                }, []);
-                setBrands(newBrands);
+                const newBrands = filteredProductsState.reduce(
+                    (acc, product) => {
+                        if (
+                            !acc.includes(product.brand) &&
+                            product.brand !== undefined
+                        ) {
+                            acc.push(product.brand)
+                        }
+                        return acc
+                    },
+                    []
+                )
+                setBrands(newBrands)
             }
         }
-    }, [brands, categories.length, filteredProductsState]);
-    
+    }, [brands, categories.length, filteredProductsState])
+
     // useEffect(() => {
-        // dispatch(updateFilters({
-        //     categories: categories || [],
-        //     brands: brands || [],
-        //     price: selectedFilters.price || '',
-        //     searchString: selectedFilters.searchString || ''
-        // }));
+    // dispatch(updateFilters({
+    //     categories: categories || [],
+    //     brands: brands || [],
+    //     price: selectedFilters.price || '',
+    //     searchString: selectedFilters.searchString || ''
+    // }));
     // }, [categories, brands, selectedFilters.price, selectedFilters.searchString, dispatch]);
-    
+
     const handleCheckboxChange = () => {
-        let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        let checkboxes = document.querySelectorAll('input[type="checkbox"]')
         let checkboxesObj = {
             categories: [],
             brands: [],
-            price: ''
-        };
-        checkboxes.forEach(checkbox => {
+            price: '',
+        }
+        checkboxes.forEach((checkbox) => {
             if (checkbox.checked) {
-                let [type, value] = checkbox.id.split('-');
+                let [type, value] = checkbox.id.split('-')
                 if (type === 'price') {
-                    checkboxesObj[type] = value;
+                    checkboxesObj[type] = value
                 } else {
-                    console.log('checkboxesObj', checkboxesObj);
-                    checkboxesObj[type].push(value);
+                    console.log('checkboxesObj', checkboxesObj)
+                    checkboxesObj[type].push(value)
                 }
             }
-        });
-        dispatch(setFilters({
-            ...checkboxesObj,
-            categories: selectedFilters.categories || [],
-            searchString: selectedFilters.searchString || ''
-        }));
-    };
-    
+        })
+        dispatch(
+            setFilters({
+                ...checkboxesObj,
+                categories: selectedFilters.categories || [],
+                searchString: selectedFilters.searchString || '',
+            })
+        )
+    }
+
     const handleClearFilters = () => {
-        dispatch(clearFilters());
+        dispatch(clearFilters())
         if (document.querySelector('.clear-icon')) {
-            document.querySelector('.clear-icon').click();
+            document.querySelector('.clear-icon').click()
         }
-    };
-    
+    }
+
     return (
         <>
-            <div className={'sidebar px-4 h-fit left-0 ' +
-                (!isMobile ? 'sticky ' : '') +
-                (isMobile ? 'block bg-white fixed top-auto bottom-0 right-0 shadow-2xl shadow-black text-xl p-6 z-10 transition ' : '') +
-                ((filtersActiveState && isMobile && !showFilters) ? 'translate-y-full' : '') +
-                ((filtersActiveState && isMobile && showFilters) ? ' ' : '')}>
+            <div
+                className={
+                    'sidebar px-4 h-fit left-0 ' +
+                    (!isMobile ? 'sticky ' : '') +
+                    (isMobile
+                        ? 'block bg-white fixed top-auto bottom-0 right-0 shadow-2xl shadow-black text-xl p-6 z-10 transition '
+                        : '') +
+                    (filtersActiveState && isMobile && !showFilters
+                        ? 'translate-y-full'
+                        : '') +
+                    (filtersActiveState && isMobile && showFilters ? ' ' : '')
+                }
+            >
                 {isMobile && (
-                    <div className="sidebar__mobile-filter-btn w-[160px] flex items-center justify-center absolute -top-10 right-0 z-50 bg-blue-500 text-white text-base font-semibold p-2 rounded-t shadow " onClick={() => setShowFilters(!showFilters)}>
+                    <div
+                        className="sidebar__mobile-filter-btn w-[160px] flex items-center justify-center absolute -top-10 right-0 z-50 bg-blue-500 text-white text-base font-semibold p-2 rounded-t shadow "
+                        onClick={() => setShowFilters(!showFilters)}
+                    >
                         {(showFilters ? 'Hide' : 'Show') + ' filters'}
                         <i className="text-lg fa-solid fa-filter pl-2"></i>
                     </div>
                 )}
-                <div className={'sidebar__filter-title text-xl font-semibold'}>Filters</div>
-                <div className={'sidebar__filter-clear text-sm font-semibold text-gray-400 cursor-pointer w-fit hover:text-blue-400 pb-4 border-b-2'} onClick={handleClearFilters}>Clear filters</div>
+                <div className={'sidebar__filter-title text-xl font-semibold'}>
+                    Filters
+                </div>
+                <div
+                    className={
+                        'sidebar__filter-clear text-sm font-semibold text-gray-400 cursor-pointer w-fit hover:text-blue-400 pb-4 border-b-2'
+                    }
+                    onClick={handleClearFilters}
+                >
+                    Clear filters
+                </div>
                 <div className={'sidebar__filter-container'}>
                     {categories.length >= 2 && (
                         <div className="sidebar__filter-section category-filter mt-2">
-                            <h3 className="text-base font-semibold text-gray-400">Categories</h3>
+                            <h3 className="text-base font-semibold text-gray-400">
+                                Categories
+                            </h3>
                             <div className="sidebar__filter-list">
                                 {categories.map((category, index) => (
-                                    <label className="w-fit flex items-center select-none gap-2" key={index} htmlFor={`category-${category}`}>
-                                        <input type="checkbox" id={`category-${category}`} onChange={handleCheckboxChange} />
-                                        <span className={'text-base'}>{capitalizeFirstLetter(category)}</span>
+                                    <label
+                                        className="w-fit flex items-center select-none gap-2"
+                                        key={index}
+                                        htmlFor={`category-${category}`}
+                                    >
+                                        <input
+                                            type="checkbox"
+                                            id={`category-${category}`}
+                                            onChange={handleCheckboxChange}
+                                        />
+                                        <span className={'text-base'}>
+                                            {capitalizeFirstLetter(category)}
+                                        </span>
                                     </label>
                                 ))}
                             </div>
@@ -115,24 +167,48 @@ export default function Sidebar({ isMobile }) {
                     )}
                     {brands.filter(Boolean).length > 0 && (
                         <div className="sidebar__filter-section brand-filter mt-2">
-                            <h3 className="text-base font-semibold text-gray-400">Brands</h3>
+                            <h3 className="text-base font-semibold text-gray-400">
+                                Brands
+                            </h3>
                             <div className="sidebar__filter-list">
                                 {brands.map((brand, index) => (
-                                    <label className="w-fit flex items-center select-none gap-2" key={index} htmlFor={`brands-${brand}`}>
-                                        <input type="checkbox" id={`brands-${brand}`} onChange={handleCheckboxChange} />
-                                        <span className={'text-base'}>{capitalizeFirstLetter(brand)}</span>
+                                    <label
+                                        className="w-fit flex items-center select-none gap-2"
+                                        key={index}
+                                        htmlFor={`brands-${brand}`}
+                                    >
+                                        <input
+                                            type="checkbox"
+                                            id={`brands-${brand}`}
+                                            onChange={handleCheckboxChange}
+                                        />
+                                        <span className={'text-base'}>
+                                            {capitalizeFirstLetter(brand)}
+                                        </span>
                                     </label>
                                 ))}
                             </div>
                         </div>
                     )}
                     <div className="sidebar__filter-section mt-2">
-                        <h3 className="text-base font-semibold text-gray-400">Price</h3>
+                        <h3 className="text-base font-semibold text-gray-400">
+                            Price
+                        </h3>
                         <div className="sidebar__filter-list">
                             {prices.map((price, index) => (
-                                <label className="w-fit flex items-center select-none gap-2" key={index} htmlFor={`price-${price.min}_${price.max}`}>
-                                    <input type="checkbox" id={`price-${price.min}_${price.max}`} onChange={handleCheckboxChange} />
-                                    <span className={'text-base'}>${price.min} - ${price.max}</span>
+                                <label
+                                    className="w-fit flex items-center select-none gap-2"
+                                    key={index}
+                                    htmlFor={`price-${price.min}_${price.max}`}
+                                >
+                                    <input
+                                        type="checkbox"
+                                        id={`price-${price.min}_${price.max}`}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <span className={'text-base'}>
+                                        ${price.min} - ${price.max}
+                                    </span>
                                 </label>
                             ))}
                         </div>
@@ -140,9 +216,5 @@ export default function Sidebar({ isMobile }) {
                 </div>
             </div>
         </>
-    );
+    )
 }
-
-Sidebar.propTypes = {
-    isMobile: PropTypes.bool
-};
