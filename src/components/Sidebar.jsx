e51445b5b2939694f07ac4170d4d7516f82bf093
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-12-13 11:55:09 -0600

    fixed search results page

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import {useSelector, useDispatch} from 'react-redux'
-import {setFilters, updateFilters, clearFilters, filtersActive} from '../store/filtersSlice'
-import {capitalizeFirstLetter} from '../utils/functions'
-import {filteredProducts} from '../store/productsSlice.js'
-import {Link} from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { setFilters, updateFilters, clearFilters, filtersActive } from '../store/filtersSlice'
+import { capitalizeFirstLetter } from '../utils/functions'
+import { filteredProducts } from '../store/productsSlice.js'
+import { Link } from 'react-router-dom'
 
-export default function Sidebar({productsArray}) {
+export default function Sidebar({ productsArray = [] }) {
     const dispatch = useDispatch()
     const isMobile = useSelector((state) => state.ui.isMobile)
     const products = useSelector((state) => state.products)
@@ -16,37 +16,35 @@ export default function Sidebar({productsArray}) {
     const [categories, setCategories] = useState([])
     const [brands, setBrands] = useState([])
     const prices = [
-        {min: 0, max: 50},
-        {min: 51, max: 100},
-        {min: 101, max: 1000},
+        { min: 0, max: 50 },
+        { min: 51, max: 100 },
+        { min: 101, max: 1000 },
     ]
     const [showFilters, setShowFilters] = useState(false)
-    const isRouteActive = (path) => location.pathname.startsWith(path);
+    const isRouteActive = (path) => location.pathname.startsWith(path)
     
     useEffect(() => {
         let productsArr = productsArray || products
-        if (Array.isArray(productsArr)) {
-            if (categories.length < 1) {
-                const newCategories = productsArr.reduce((acc, product) => {
-                    if (!acc.includes(product.category) && product.category !== undefined) {
-                        acc.push(product.category)
-                    }
-                    return acc
-                }, [])
-                setCategories(newCategories)
-            }
-            if (brands.length < 1) {
-                const newBrands = filteredProductsState.reduce((acc, product) => {
-                    if (!acc.includes(product.brand) && product.brand !== undefined) {
-                        acc.push(product.brand)
-                    }
-                    return acc
-                }, [])
-                setBrands(newBrands)
-            }
+        if (categories.length < 1) {
+            const newCategories = products.reduce((acc, product) => {
+                if (!acc.includes(product.category) && product.category !== undefined) {
+                    acc.push(product.category)
+                }
+                return acc
+            }, [])
+            setCategories(newCategories)
         }
-    }, [filteredProductsState, categories.length, brands.length])
-
+        if (brands.length !== productsArray.length) {
+            const newBrands = productsArray.reduce((acc, product) => {
+                if (!acc.includes(product.brand) && product.brand !== undefined) {
+                    acc.push(product.brand)
+                }
+                return acc
+            }, [])
+            setBrands(newBrands)
+        }
+    }, [productsArray, categories.length, brands.length, products])
+    
     const handleCheckboxChange = () => {
         let checkboxes = document.querySelectorAll('input[type="checkbox"]')
         let checkboxesObj = {
@@ -69,7 +67,7 @@ export default function Sidebar({productsArray}) {
                 ...checkboxesObj,
                 categories: selectedFilters.categories || [],
                 searchString: selectedFilters.searchString || '',
-            })
+            }),
         )
     }
     const handleClearFilters = () => {
@@ -79,9 +77,9 @@ export default function Sidebar({productsArray}) {
         }
     }
     const handleLinkClick = (category) => {
-        dispatch(updateFilters({categories: [category]}))
+        dispatch(updateFilters({ categories: [category] }))
     }
-
+    
     return (
         <>
             <div className={'sidebar px-4 h-fit left-0 min-w-[300px] w-1/5' +
@@ -89,16 +87,16 @@ export default function Sidebar({productsArray}) {
                 (isMobile ? 'block bg-white fixed top-auto bottom-0 right-0 shadow-2xl shadow-black text-xl p-6 z-10 transition ' : '') +
                 (filtersActiveState && isMobile && !showFilters ? 'translate-y-full' : '') + (filtersActiveState && isMobile && showFilters ? ' ' : '')}>
                 {isMobile && (
-                    <div className="sidebar__mobile-filter-btn w-[160px] flex items-center justify-center absolute -top-10 right-0 z-50 bg-blue-500 text-white text-base font-semibold p-2 rounded-t shadow " onClick={() => setShowFilters(!showFilters)}>
+                    <div className={'sidebar__mobile-filter-btn w-[160px] flex items-center justify-center absolute -top-10 right-0 z-50 bg-blue-500 text-white text-base font-semibold p-2 rounded-t shadow '} onClick={() => setShowFilters(!showFilters)}>
                         {(showFilters ? 'Hide' : 'Show') + ' filters'}
-                        <i className="text-lg fa-solid fa-filter pl-2"></i>
+                        <i className={'text-lg fa-solid fa-filter pl-2'}></i>
                     </div>
                 )}
                 <div className={'sidebar__filter-title text-xl font-semibold'}>Filters</div>
                 <div className={'sidebar__filter-container'}>
-                    <div className="sidebar__filter-section category-filter mt-2">
-                        <h3 className="text-base font-semibold text-gray-400">Categories</h3>
-                        <div className="sidebar__filter-list mt-2">
+                    <div className={'sidebar__filter-section category-filter mt-2'}>
+                        <h3 className={'text-base font-semibold text-gray-400'}>Categories</h3>
+                        <div className={'sidebar__filter-list mt-2'}>
                             {categories.map((category, index) => (
                                 <Link to={`/category/${category}`} key={index} className={'w-fit flex text-blue-600 hover:underline items-center select-none gap-2'} onClick={() => (handleLinkClick(category))}>
                                     <span className={'text-base'}>{capitalizeFirstLetter(category)}</span>
@@ -107,24 +105,36 @@ export default function Sidebar({productsArray}) {
                         </div>
                     </div>
                     {brands.filter(Boolean).length > 0 && (
-                        <div className="sidebar__filter-section brand-filter mt-2">
-                            <h3 className="text-base font-semibold text-gray-400">Brands</h3>
-                            <div className="sidebar__filter-list">
+                        <div className={'sidebar__filter-section brand-filter mt-2'}>
+                            <h3 className={'text-base font-semibold text-gray-400'}>Brands</h3>
+                            <div className={'sidebar__filter-list'}>
                                 {brands.map((brand, index) => (
-                                    <label className="w-fit flex items-center select-none gap-2" key={index} htmlFor={`brands-${brand}`}>
-                                        <input type="checkbox" id={`brands-${brand}`} onChange={handleCheckboxChange} />
+                                    <label className={'w-fit flex items-center select-none gap-2'} key={index} htmlFor={`brands-${brand}`}>
+                                        <input type={'checkbox'} id={`brands-${brand}`} onChange={handleCheckboxChange} />
                                         <span className={'text-base'}>{capitalizeFirstLetter(brand)}</span>
                                     </label>
                                 ))}
                             </div>
                         </div>
                     )}
-                    <div className="sidebar__filter-section mt-2">
-                        <h3 className="text-base font-semibold text-gray-400">Price</h3>
-                        <div className="sidebar__filter-list">
+                    <div className={'sidebar__filter-section mt-2'}>
+                        <h3 className={'text-base font-semibold text-gray-400'}>Price</h3>
+                        <div className={'sidebar__filter-list'}>
                             {prices.map((price, index) => (
-                                <label className="w-fit flex items-center select-none gap-2" key={index} htmlFor={`price-${price.min}_${price.max}`}>
-                                    <input type="checkbox" id={`price-${price.min}_${price.max}`} onChange={handleCheckboxChange} />
+                                <label className={'w-fit flex items-center select-none gap-2'} key={index} htmlFor={`price-${price.min}_${price.max}`}>
+                                    <input
+                                        type={'checkbox'}
+                                        id={`price-${price.min}_${price.max}`}
+                                        onChange={(e) => {
+                                            // Uncheck all other checkboxes
+                                            document.querySelectorAll('input[type="checkbox"][id^="price-"]').forEach((checkbox) => {
+                                                if (checkbox !== e.target) {
+                                                    checkbox.checked = false
+                                                }
+                                            })
+                                            handleCheckboxChange()
+                                        }}
+                                    />
                                     <span className={'text-base'}>
                                         ${price.min} - ${price.max}
                                     </span>
--- src/pages/SearchResultsView.jsx
+++ src/pages/SearchResultsView.jsx
@@ -5,44 +5,82 @@ import { scrollToTop } from '../utils/functions.jsx'
 import store from '../store.js'
 import { updateFilters, clearFilters } from '../store/filtersSlice.js'
 import Sidebar from '../components/Sidebar.jsx'
+import { arrayMatch } from '../utils/functions.jsx'
 
 export default function SearchResultsView() {
     const dispatch = store.dispatch
-    const products = useSelector(state => state.products);
-    const searchString = useSelector(state => state.filters.searchString);
+    const products = useSelector(state => state.products)
+    const searchString = useSelector(state => state.filters.searchString)
+    const selectedBrands = useSelector(state => state.filters.brands)
+    const selectedPrice = useSelector(state => state.filters.price)
+    const [staticSearchString, setStaticSearchString] = useState('')
+    const [staticSearchResults, setStaticSearchResults] = useState([])
+    const [sideBarFilters, setSideBarFilters] = useState([])
+    const [staticFilters, setStaticFilters] = useState({ brands: [], price: '' })
     const [initialClear, setInitialClear] = useState(false)
-    const filteredProducts = useMemo(() => {
-        return products.filter(product => {
-            return product.title.toLowerCase().includes(searchString.toLowerCase())
-        })
-    }, [products, searchString])
     
+    //  Update Static Search Results
     useEffect(() => {
         scrollToTop()
-        // if search string is empty, update filters with search string
         if (!searchString) {
-            console.log('search string is empty, updating filters with search string')
             const search = window.location.pathname.split('/').pop()
-            dispatch(updateFilters({searchString: search}))
+            setStaticSearchString(search)
+        } else {
+            setStaticSearchString(searchString)
         }
+        
+        // First filter products by search string
+        const sidebarResults = products.filter(product => {
+            return product.title.toLowerCase().includes(staticSearchString.toLowerCase())
+        })
+        setSideBarFilters(sidebarResults)
+        
+        const searchResults = products.filter(product => {
+            let brandMatch = staticFilters.brands.length < 1 || staticFilters.brands.includes(product.brand)
+            const [min, max] = staticFilters.price.split('_')
+            let priceMatch = !staticFilters.price || (product.price >= min && product.price <= max)
+            let searchMatch = product.title.toLowerCase().includes(staticSearchString.toLowerCase())
+            return brandMatch && priceMatch && searchMatch
+        })
+        setStaticSearchResults(searchResults)
+        
         // if initialClear is false, clear filters
-        if (!initialClear) {
-            console.log('clearing filters and setting initialClear to true')
+        if (!initialClear && staticSearchString && staticSearchResults.length > 0) {
             dispatch(clearFilters())
             setInitialClear(true)
         }
-    }, [dispatch, searchString])
+    }, [dispatch, initialClear, products, searchString, staticFilters.brands, staticFilters.price, staticSearchResults.length, staticSearchString])
+    
+    // Update Static Filters
+    useEffect(() => {
+        if (!arrayMatch(selectedBrands, staticFilters.brands)) {
+            setStaticFilters({ ...staticFilters, brands: selectedBrands })
+        }
+        if (staticFilters.price !== selectedPrice) {
+            setStaticFilters({ ...staticFilters, price: selectedPrice })
+        }
+        if (searchString && staticSearchString !== searchString) {
+            setStaticSearchString(searchString)
+        }
+    }, [staticSearchString, selectedBrands, selectedPrice, staticFilters, searchString])
     
     return (
         <div className={'results-view'}>
-            <h1 className={'results-view__heading opacity-0'}>test</h1>
+            <h1 className={'results-view__heading'}>{`Results for "${staticSearchString}"`}</h1>
             <div className={'results-view__content'}>
-                <Sidebar products={filteredProducts}/>
-                <div className="results-view__content__grid">
-                    {filteredProducts.map(product => (
-                        <ProductCard key={product.id} product={product} />
-                    ))}
-                </div>
+                <Sidebar productsArray={sideBarFilters} />
+                {staticSearchResults.length > 0 && (
+                    <div className={'results-view__content__grid'}>
+                        {staticSearchResults.map(product => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                    </div>
+                )}
+                {staticSearchResults.length < 1 && (
+                    <div className={'results-view__no-results flex items-center justify-center absolute-centered'}>
+                        <h2 className={'text-xl'}>No results ¯\_(ツ)_/¯</h2>
+                    </div>
+                )}
             </div>
         </div>
     )
