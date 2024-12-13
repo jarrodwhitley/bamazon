f8030cd5bebf5ed07046b7d831a03dceeb8a56dd
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-12-13 11:55:09 -0600

    fixed search results page

--- src/utils/functions.jsx
+++ src/utils/functions.jsx
@@ -41,3 +41,7 @@ export const scrollToTop = () => {
         })
     }
 }
+
+export const arrayMatch = (arr1, arr2) => {
+    return arr1.length === arr2.length && arr1.every((val, index) => val === arr2[index])
+}
