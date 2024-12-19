0ae24f7ef2c506e22bc1fc76d28e7809b47632dc
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-12-19 13:40:20 -0600

    more updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import store from './store.js'
 import './index.css'
 import App from './App.jsx'
+import { Buffer } from 'buffer';
+
+window.Buffer = Buffer;
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import NodePolyfills from 'vite-plugin-node-polyfills';
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    resolve: {
+        alias: {
+            buffer: 'buffer',
+        },
+    },
+    define: {
+        'process.env': {},
+        global: 'globalThis',
+    },
+    plugins: [
+        react(),
+        NodePolyfills({
+            protocolImports: true,
+        }),],
     build: {
         outDir: 'build',
     },
