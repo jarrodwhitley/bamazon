6b46b62bc6781a8cc5457c088b754916cdc1ed14
Jarrod Whitley <jarrodwhitley@gmail.com>
2025-01-13 13:38:20 -0600

    updated firebaseadmin

--- src/firebaseAdmin.js
+++ src/firebaseAdmin.js
@@ -2,11 +2,10 @@ import admin from "firebase-admin";
 import dotenv from "dotenv";
 dotenv.config();
 
-const serviceAccount = JSON.parse(process.env.VITE_FIREBASE_SERVICE_ACCOUNT);
-console.log('serviceAccount', serviceAccount)
+const serviceAccount = JSON.parse(import.meta.env.VITE_FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.VITE_FIREBASE_DATABASE_URL
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL
 });
 
 export { admin }
