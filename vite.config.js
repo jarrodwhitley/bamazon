094830447c568d0a237c28b1289e056398971285
Jarrod Whitley <jarrodwhitley@gmail.com>
2025-01-13 13:17:43 -0600

    WIP

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path';
-import NodePolyfills from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            buffer: 'buffer',
-        },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
     },
-    define: {
-        'process.env': {},
-        global: 'globalThis',
+  },
+  define: {
+    'process.env': {},
+    global: 'globalThis',
+  },
+  plugins: [
+    react(),
+    nodePolyfills({
+      protocolImports: true,
+    }),
+  ],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
     },
-    plugins: [
-        react(),
-        NodePolyfills({
-            protocolImports: true,
-        }),],
-    build: {
-        outDir: 'build',
-    },
+  },
 })
